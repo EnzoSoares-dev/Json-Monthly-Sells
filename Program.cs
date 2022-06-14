27c31f22ae6384fb5537b776d62662edda6d6ee6
Enzo Soares <soaressenzo@gmail.com>
2022-06-14 14:59:09 -0300

    Created

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace Question_3{
+
+    public class Program{
+        static void Main(){
+            double MenorValor=0,MaiorValor=0, DiaMaior=0,media=0, total=0;
+            int Count=0;
+            StreamReader reader = new StreamReader("dados.json");            
+            string jsonToString = reader.ReadToEnd();            
+            data[]? datas = JsonConvert.DeserializeObject<data[]>(jsonToString);                               
+
+            for(int i=0;i<datas.Length;i++){                
+                total += double.Parse(datas[i].valor.ToString("n2"));
+                if(datas[i].valor !=0){
+                    Count++;
+                }
+                if(MaiorValor==0 && MenorValor == 0){
+                    MaiorValor = datas[i].valor;
+                    MenorValor = datas[i].valor;
+                }else{
+                    if(datas[i].valor > MaiorValor){
+                        MaiorValor = datas[i].valor;
+                    }
+                    if(datas[i].valor < MenorValor){
+                        MenorValor = datas[i].valor;
+                    }
+                }                
+            }
+            media = total/Count;
+
+            for(int i=0; i<datas.Length;i++){
+                if(datas[i].valor>media){
+                    DiaMaior++;
+                }
+            }
+
+            System.Console.WriteLine("Média Mensal: "+media);
+            System.Console.WriteLine("Maior valor de venda: "+MaiorValor);
+            System.Console.WriteLine("Menor valor de venda: "+MenorValor);
+            System.Console.WriteLine("Número de dias com valor maior que a média: "+DiaMaior);
+        }
+    }
+}
---
+++ data.cs
@@ -0,0 +1,6 @@
+namespace Question_3{
+    public class data{
+        public int dia { get; set; }
+        public double valor { get; set; }
+    }
+}
